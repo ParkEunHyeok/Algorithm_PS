2b27b3db321a50a090c62468b95c1bd45511519f
ParkEunHyeok <tomas3630@gmail.com>
2020-11-10 21:33:32 +0900

    1252 continue

--- 01_ProblemSolving/1252.c
+++ 01_ProblemSolving/1252.c
@@ -2,8 +2,10 @@
 
 int main() {
     char arr[2][81];
-    int result;
+    int sum = 0;
     scanf("%s %s", arr[0], arr[1]);
+
+    // 이진수를 십진수로 바꾸고 더함.
     for(int i=0; i<2; i++) {
         int number = 0;
         for(int j=0; arr[i][j]!=NULL; j++) {
@@ -13,9 +15,18 @@ int main() {
                 number += 1;
             }
         }
-        result = number;
+        sum += number;
     }
 
-    printf("%d", result);
+    // 총합을 이진수로 바꿈.
+    if((sum%2)==0) printf("1");
+    sum = sum / 2;
+    while(1) {
+        if(sum<1) break;
+        if((sum%2)==0) printf("0");
+        else printf("1");
+        sum = sum/2;
+    }
+
     return 0;
 }
