2496a7b3e116e8647770b346bb964e960770f352
ParkEunHyeok <tomas3630@gmail.com>
2020-11-21 17:22:50 +0900

    15311 solved

--- 01_ProblemSolving/15311.c
+++ 01_ProblemSolving/15311.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int result = 0;
-    for(int i=1; i<=2000; i++) {
-        result += i;
+    printf("2000\n");
+    for(int i=0; i<1000; i++) {
+        printf("1 ");
     }
-    printf("%d", result);
+    for(int i=0; i<1000; i++) {
+        printf("999 ");
+    }
     return 0;
 }
