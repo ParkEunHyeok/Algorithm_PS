f197dea5fa495fdf95c9428a10dbbefcd4c5f745
ParkEunHyeok <tomas3630@gmail.com>
2021-01-03 01:10:04 +0900

    11000

---
+++ 01_ProblemSolving/11000.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<pair<int, int> > v;
+    priority_queue<int, vector<int>, greater<int> > pq;
+
+    v.resize(n);
+    for(int i=0; i<n; i++) {
+        cin >> v[i].first >> v[i].second;
+    }
+    sort(v.begin(), v.end());
+    pq.push(v[0].second);
+
+    for(int i=1; i<n; i++) {
+        if(pq.top() <= v[i].first) {
+            pq.pop();
+            pq.push(v[i].second);
+        }
+        else {
+            pq.push(v[i].second);
+        }
+    }
+
+    cout << pq.size();
+    return 0;
+}
