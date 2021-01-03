27353b059f533b911c5aa9f50884c9e914c9b646
ParkEunHyeok <tomas3630@gmail.com>
2021-01-04 01:42:47 +0900

    1920 solved

--- 01_ProblemSolving/1920.cpp
+++ 01_ProblemSolving/1920.cpp
@@ -1,8 +1,37 @@
-#include <stdio.h>
-#include <cstdio>
+#include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
-int main()
-{
-    
+
+vector<int> v;
+
+int binSearch(int low, int high, int target) {
+    if(low > high) return 0;
+    else {
+        int mid = (low+high)/2;
+        if(v[mid]==target) return 1;
+        else if(v[mid] > target) return binSearch(low, mid-1, target);
+        else return binSearch(mid+1, high, target);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n;
+    cin >> n;
+    v.resize(n);
+    for(int i=0; i<n; i++) {
+        cin >> v[i];
+    }  
+    sort(v.begin(), v.end());
+
+    int m;
+    cin >> m;
+    for(int i=0; i<m; i++) {
+        int num;
+        cin >> num;
+        cout << binSearch(0, n-1, num) << "\n";
+    }
+
+    return 0;
 }
