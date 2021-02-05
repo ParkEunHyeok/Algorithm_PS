68d1009d22597a224c301edaffc9b92b63da3602
ParkEunHyeok <tomas3630.gmail.com>
2021-02-06 01:11:15 +0900

    1946 solved

--- 01_ProblemSolving/1946.cpp
+++ 01_ProblemSolving/1946.cpp
@@ -1,39 +1,33 @@
 #include <iostream>
-#include <cstring>
+#include <algorithm>
+#include <vector>
 using namespace std;
-
-int first[100002];
-int second[100002];
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    
     int t;
     cin >> t;
     while(t--) {
-        int n;
-        memset(first, 0, sizeof(first));
-        memset(second, 0, sizeof(second));
-        int first_max_idx=0, second_max_idx=0;
+        int n, num1, num2;
+        vector<pair<int, int> > v;
         cin >> n;
-        
-        for(int i=0; i<n; i++) {
-            cin >> first[i] >> second[i];
-            if(first[i]==1) first_max_idx = i;
-            if(second[i]==1) second_max_idx = i;
-        }
 
-        int result = 0;
         for(int i=0; i<n; i++) {
-            if(first[i]==1 || second[i]==1) result++;
-            else if(first[i]<first[second_max_idx] && second[i]<second[first_max_idx]) {
-                result ++;
+            cin >> num1 >> num2;
+            v.push_back(make_pair(num1, num2));
+        }
+        sort(v.begin(), v.end());
+
+        int cnt = 0, tmp=1000000;
+        for(int i=0; i<n; i++) {
+            if(v[i].second < tmp) {
+                tmp = v[i].second;
+                cnt++;
             }
         }
 
-        cout << result << "\n";
+        cout << cnt << "\n";
     }
-
     return 0;
 }
