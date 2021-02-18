f2e0aa2d879beeb3eb8bd5e388789cbe5b167f48
ParkEunHyeok <tomas3630.gmail.com>
2021-02-19 01:35:15 +0900

    17609 solved

--- 01_ProblemSolving/17609.cpp
+++ 01_ProblemSolving/17609.cpp
@@ -2,35 +2,67 @@
 #include <string>
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
+int isPalindrome(string str, int skip) {
+    int start = 0;
+    int end = str.length()-1;
 
-    while(n--) {
-        string str;
-        cin >> str;
-        int len = str.length();
+    while(start < end) {
+        if(start==skip) start++;
+        if(end==skip) end--;
 
-        int start = 0;
-        int end = len-1;
-        int ans = 0;
-
-        for(int i=0; i<(len/2); i++) {
-            if(ans==2) break;
-            
-            if(str[start]==str[end]) {
-                start++;
-                end--;
-            }
-            else {
-                ans++;
-                start++;
-                end--;
-            }
+        if(str[start]!=str[end]) {
+            return 2;
         }
 
-        cout << ans << "\n";
+        start++;
+        end--;
     }
+
+    return 1;
+}
+
+int discriminant(string str) {
+    int start = 0;
+    int end = str.length()-1;
+    int ans = 0;
+
+    while(start < end) {
+        if(ans==2) break;
+
+        if(str[start]!=str[end]) {
+            ans = 2;
+
+            if(str[start+1]==str[end]) {
+                ans = min(ans, isPalindrome(str, start));
+            }
+
+            if(str[start]==str[end-1]) {
+                ans = min(ans, isPalindrome(str, end));
+            }
+            
+            return ans;
+        }
+
+        start++;
+        end--;
+    }
+
+    return 0;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n;
+    cin >> n;
     
+    while(n--) {
+        string s;
+        cin >> s;
+
+        cout << discriminant(s) << "\n";
+    }
+
     return 0;
 }
