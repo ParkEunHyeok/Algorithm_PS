3bb43d9ceecaf8debf9ad83746f01014960f721a
ParkEunHyeok <tomas3630@gmail.com>
2021-05-19 22:25:35 +0900

    18870 solved

--- 01_ProblemSolving/18870.cpp
+++ 01_ProblemSolving/18870.cpp
@@ -8,34 +8,22 @@ int main() {
     cin.tie(0); cout.tie(0);
 
     int n;
-    vector<int> arr;
-    vector<int> v;
+    vector<int> arr, v;
 
     cin >> n;
-
     for(int i=0; i<n; i++) {
         int num;
         cin >> num;
         v.push_back(num);
         arr.push_back(num);
     }
+
     sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
 
     for(int i=0; i<n; i++) {
-        int cur = arr[i];
-        int prev = 987654321;
-        int a = 0;
-
-        for(int j=0; j<n; j++) {
-            if(cur > v[j]) {
-                if(v[j]==prev) continue;
-                a++;
-                prev = v[j];
-            }   
-            else break;
-        }
-
-        cout << a << " ";
+        int out = lower_bound(v.begin(), v.end(), arr[i]) - v.begin();
+        cout << out << " ";
     }
 
     return 0;
