09112c9f98f0c505997fd438b57ce9aa4eb662d6
ParkEunHyeok <tomas3630@gmail.com>
2021-05-26 08:17:18 +0900

    11003 solved:

--- 01_ProblemSolving/11003.cpp
+++ 01_ProblemSolving/11003.cpp
@@ -3,24 +3,25 @@
 using namespace std;
 #define pii pair<int, int>
 
-bool compare(pii a, pii b) {
-    
-}
-
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
     int n, d;
-    int lo, hi;
     priority_queue<pii, vector<pii>, greater<pii> > pq;
 
     cin >> n >> d;
     for(int i=1; i<=n; i++) {
-        lo = i + 1 - d;
-        hi = i;
-
         int temp;
         cin >> temp;
+        pq.push({temp, i});
+
+        while(pq.top().second < (i-d+1)) {
+            pq.pop();
+        }
+
+        cout << pq.top().first << " ";
     }
+
+    return 0;
 }
