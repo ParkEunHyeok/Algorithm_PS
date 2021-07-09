30d80ecbb02ccdc5a23ec542d890c926440c3ce8
ParkEunHyeok <tomas3630@gmail.com>
2021-07-10 00:38:35 +0900

    1987 solved

--- 01_ProblemSolving/1987.cpp
+++ 01_ProblemSolving/1987.cpp
@@ -2,35 +2,32 @@
 #include <cstring>
 using namespace std;
 
+// Algorithm : DFS, Backtracking
+// Logic : (0,0)을 방문처리한 후 DFS 실행.
+//         DFS를 돌릴 때 방문 안했으면 돌리고,
+//         돌린 후에 방문 false로 바꾸기 (백트래킹)
+
 int r, c;
 char map[22][22];
-bool visited[22][22];
-bool alphabet[22];
+bool alphabet[28];
 int dir[4][2] = {{0,1},{0,-1},{1,0},{-1,0}};
 int result;
 
-int init() {
-    memset(visited, 0, sizeof(visited));
-    memset(alphabet, 0, sizeof(alphabet));
-}
-
-int dfs(int y, int x) {
-    int depth = 1;
-    visited[y][x] = true;
-    alphabet[map[y][x]-'A'] = true;
+void dfs(int y, int x, int depth) {
+    result = max(result, depth);
 
     for(int i=0; i<4; i++) {
         int nextY = y + dir[i][0];
         int nextX = x + dir[i][1];
 
-        if(nextY<0 || nextX<0 || nextY>=r || nextY>=c) continue;
-        if(alphabet[map[nextY][nextX]-'A']) continue;
-        if(!visited[nextY][nextX]) {
-            depth += dfs(nextY, nextX);
+        if(nextY<0 || nextX<0 || nextY>=r || nextX>=c) continue;
+        
+        if(!alphabet[map[nextY][nextX]-'A']) {
+            alphabet[map[nextY][nextX]-'A'] = true;
+            dfs(nextY, nextX, depth+1);
+            alphabet[map[nextY][nextX]-'A'] = false;
         }
     }
-
-    return depth;
 }
 
 int main () {
@@ -43,20 +40,12 @@ int main () {
         cin >> temp;
 
         for(int j=0; j<c; j++) {
-           map[i][j] = temp[j];
+            map[i][j] = temp[j];
         }
     }
-    
-    for(int i=0; i<r; i++) for(int j=0; j<c; j++) {
-        init();
-        int temp = dfs(i, j) - 1;
-        if (result < temp)
-        {
-           result = temp;
-           cout << i << ' '<< j << '\n'; 
-        }
-        // result = max(result, dfs(i,j)-1);
-    }
+
+    alphabet[map[0][0]-'A'] = true;
+    dfs(0,0,1);
 
     cout << result;
     return 0;
