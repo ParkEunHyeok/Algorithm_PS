e0cbfa828402f4e91627a6b037046c7759c1c66f
ParkEunHyeok <tomas3630@gmail.com>
2021-12-24 12:52:54 +0900

    19621 solved:

--- 01_ProblemSolving/23814.cpp
+++ 01_ProblemSolving/23814.cpp
@@ -3,30 +3,23 @@ using namespace std;
 #define ll long long
 
 int main() {
-    ll d, n, m, k;
+    ll d, n, m, k, res;
     cin >> d >> n >> m >> k;
 
-    ll jja = d - (n%d);
-    ll jjam = d - (m%d);
-    ll bok = (k%d);
-    ll result = d*(k/d);
+    ll a1 = d - (n%d);
+    ll a2 = d - (m%d);
 
-    if(bok >= jja + jjam) {
-        result += bok - jja - jjam;
-    } else if(bok >= jja || bok >= jjam) {
-        if(bok >= jja && bok >= jjam) {
-            result += min(bok-jja, bok-jjam);
-        } else {
-            if(bok >= jja) result += bok-jja;
-            else result += bok-jjam;
-        }
-    } else if(bok+d >= jja + jjam && (k/d) >= 1) {
-        result -= d;
-        result += bok + d - jja - jjam;
-    } else {
-        result += bok;
-    }
+    if(a1 > a2) swap(a1, a2);
 
-    cout << result;
+    ll ans1 = k - a1 - a2;
+    ll ans2 = k - a1;
+    ll ans3 = k;
+
+    if(ans1/d+2 == ans2/d+1) {
+        if(ans3/d == ans1/d+2) res = ans3;
+        else res = ans2;
+    } else res = ans1;
+
+    cout << res;
     return 0;
 }
