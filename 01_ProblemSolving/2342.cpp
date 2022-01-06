da50294fd65df90b12fdfc7051126bb99a653b46
ParkEunHyeok <tomas3630@gmail.com>
2022-01-07 01:20:37 +0900

    2342 solved

--- 01_ProblemSolving/2342.cpp
+++ 01_ProblemSolving/2342.cpp
@@ -3,24 +3,24 @@
 using namespace std;
 #define INF 987654321
 
-int jump[5][5] = {{1,0,0,0,0},
-                  {2,1,3,4,3},
-                  {2,3,1,3,4},
-                  {2,4,3,1,3},
-                  {2,3,4,3,1}};
-int len = 0;
+int jump[5][5] = {{1,2,2,2,2},
+                  {0,1,3,4,3},
+                  {0,3,1,3,4},
+                  {0,4,3,1,3},
+                  {0,3,4,3,1}};
+int len = 1;
 int step[100004];
 int cache[100004][5][5];
 
 int dp(int idx, int left, int right) {
-    if(idx==len) return 0;
+    if(idx==len-1) return 0;
     if((left!=0 && right!=0) && (left==right)) return INF;
 
     int &ret = cache[idx][left][right];
     if(ret!=-1) return ret;
 
-    return ret = min(dp(idx+1,step[idx+1],right)+jump[step[idx+1]][left],
-                     dp(idx+1,left,step[idx+1])+jump[step[idx+1]][right]);
+    return ret = min(dp(idx+1,step[idx+1],right)+jump[left][step[idx+1]],
+                     dp(idx+1,left,step[idx+1])+jump[right][step[idx+1]]);
 }
 
 int main() {
