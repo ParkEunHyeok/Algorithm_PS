5ef269a15ba0e0c0cffaa2f423662c7c1dcda3de
ParkEunHyeok <tomas3630@gmail.com>
2022-01-18 01:47:11 +0900

    1918 solved

--- 01_ProblemSolving/1918.cpp
+++ 01_ProblemSolving/1918.cpp
@@ -6,24 +6,38 @@ int main() {
     string str;
     cin >> str;
 
-    int status = 0; // 상태 1:다음 수식 push, 2:다음 수식 pop
     stack<char> s;
+    
     for(int i=0; i<str.length(); i++) {
-        if(str[i]=='(') {
-            status++;
-        }
-        else if(str[i]==')') {
-            cout << s.top();
-            status--;
-            s.pop();
-        }
-        else if(str[i]=='+' || str[i]=='-' || str[i]=='*' || str[i]=='/') {
-            s.push(str[i]);
-        }
-        else {  // 피연산자
-            cout << str[i];
+        if('A'<=str[i] && str[i]<='Z') cout << str[i];
+        else {
+            if(str[i]=='*' || str[i]=='/') {
+                while(!s.empty() && (s.top()=='*' || s.top()=='/')) {
+                    cout << s.top();
+                    s.pop();
+                }
+                s.push(str[i]);
+            }
+            else if(str[i]=='+' || str[i]=='-') {
+                while(!s.empty() && s.top()!='(') {
+                    cout << s.top();
+                    s.pop();
+                }
+                s.push(str[i]);
+            }
+            else if(str[i]=='(') {
+                s.push(str[i]);
+            }
+            else if(str[i]==')'){
+                while(!s.empty() && s.top()!='(') {
+                    cout << s.top();
+                    s.pop();
+                }
+                s.pop();
+            }
         }
     }
+
     while(!s.empty()) {
         cout << s.top();
         s.pop();
