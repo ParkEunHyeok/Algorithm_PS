d53fb441311d1f4f0ec3a4cc16174d6e8582ccd8
ParkEunHyeok <tomas3630@gmail.com>
2022-02-01 00:24:20 +0900

    14503 solved

--- 01_BOJ/14503.cpp
+++ 01_BOJ/14503.cpp
@@ -1,57 +1,44 @@
 #include <iostream>
 using namespace std;
 
-int n, m, r, c, d, res=0;
+int n, m, y, x, d, res=0;
 int map[52][52];
-
-void rotate() {
-    int tmp[52][52];
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<m; j++) {
-            tmp[i][j] = map[i][j];
-        }
-    }
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<m; j++) {
-            map[j][n-1-i] = tmp[i][j];
-        }
-    }
-    swap(n, m);
-}
-
-void robot(int y, int x) {
-    if(!map[y][x]) {
-        res++;
-        map[y][x] = 2;
-    }
-
-    for(int i=0; i<4; i++) {
-        if(x-1>=0 && map[y][x-1]==0) {
-            x--;
-            rotate();
-            robot(y, x);
-            return;
-        } else rotate();
-    }
-    
-    if(y+1<n && map[y+1][x]==1) return;
-    else robot(y+1, x);
-}
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
     cin >> n >> m;
-    cin >> r >> c >> d;
+    cin >> y >> x >> d;
     for(int i=0; i<n; i++) {
         for(int j=0; j<m; j++) {
             cin >> map[i][j];
         }
     }
 
-    for(int i=0; i<d; i++) rotate();
-    robot(r, c);
+    while(1) {
+        if(map[y][x]==0) res++;
+        map[y][x] = 2;
+
+        bool cleanable = false;
+        for(int i=0; i<4; i++) {
+            d = (d+3)%4;
+            if(map[y+dy[d]][x+dx[d]]==0) {
+                y += dy[d];
+                x += dx[d];
+                cleanable = true;
+                break;
+            }
+        }
+        if(cleanable) continue;
+
+        // 마지막 행, 열은 벽이므로 범위 처리 불필요
+        if(map[y-dy[d]][x-dx[d]]==1) break;
+        x -= dx[d];
+        y -= dy[d];
+    }
+
     cout << res;
-    return 0;
 }
