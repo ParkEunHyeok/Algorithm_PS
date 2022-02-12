2c66398ac2acd9cb25b69e9aeb6c0ef7e25f3701
ParkEunHyeok <tomas3630@gmail.com>
2022-02-12 16:13:28 +0900

    19598 continue

---
+++ 01_BOJ/19598.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+using namespace std;
+typedef pair<int, int> pii;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n, s, e;
+    vector<pii> v;
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        cin >> s >> e;
+        v.push_back({e, s});
+    }
+    sort(v.begin(), v.end());
+
+    int res = 1;
+    pq.push(v[0].first);
+    for(int i=1; i<n; i++) {
+        while(!pq.empty() && (pq.top()<=v[i].first))
+            pq.pop();
+        pq.push(v[i].first);
+        res = max(res, (int)pq.size());
+    }
+
+    cout << res;
+    return 0;
+}
