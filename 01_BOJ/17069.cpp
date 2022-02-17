ac575ed4673e4a9fcaa5baa11253200c7289f4f9
ParkEunHyeok <tomas3630@gmail.com>
2022-02-18 01:44:27 +0900

    17069 solved

--- 01_BOJ/17069.cpp
+++ 01_BOJ/17069.cpp
@@ -7,33 +7,35 @@ int n;
 int map[34][34];
 ll cache[3][34][34];
 
+bool OOB(int y, int x) {
+    return (y<n && y>=0 && x<n && x>=0);
+}
+
 ll func(int dir, int y, int x) {
-    if(y==n-1 && dir==1) return 0;
-    if(x==n-1 && dir==0) return 0;
-    if(y==n-1 && x==n-1) return 1; 
+    if(y==n-1 && x==n-1) return 1;
 
     ll& ret = cache[dir][y][x];
     if(ret!=-1) return ret;
     ret = 0;
 
     if(dir==0) {
-        if(map[y][x+1]==0)
+        if(map[y][x+1]==0 && OOB(y, x+1))
             ret += func(0, y, x+1);
-        if(map[y+1][x+1]==0 && map[y+1][x]==0 && map[y][x+1]==0)
+        if(map[y+1][x+1]==0 && map[y+1][x]==0 && map[y][x+1]==0 && OOB(y+1, x+1))
             ret += func(2, y+1, x+1);
     }
     else if(dir==1) {
-        if(map[y+1][x]==0)
+        if(map[y+1][x]==0 && OOB(y+1, x))
             ret += func(1, y+1, x);
-        if(map[y+1][x+1]==0 && map[y+1][x]==0 && map[y][x+1]==0)
+        if(map[y+1][x+1]==0 && map[y+1][x]==0 && map[y][x+1]==0 && OOB(y+1, x+1))
             ret += func(2, y+1, x+1);
     }
     else {
-        if(map[y][x+1]==0)
-            ret += func(0, y, x+1);
-        if(map[y+1][x]==0)
+        if(map[y][x+1]==0 && OOB(y, x+1))
+            ret += func(0, y, x+1); 
+        if(map[y+1][x]==0 && OOB(y+1, x))
             ret += func(1, y+1, x);
-        if(map[y+1][x+1]==0 && map[y+1][x]==0 && map[y][x+1]==0)
+        if(map[y+1][x+1]==0 && map[y+1][x]==0 && map[y][x+1]==0 && OOB(y+1, x+1))
             ret += func(2, y+1, x+1);
     }
 
@@ -52,6 +54,7 @@ int main() {
     }
     memset(cache, -1, sizeof(cache));
 
-    cout << func(0, 0, 1);
+    if(map[n-1][n-1]==1) cout << 0;
+    else cout << func(0, 0, 1);
     return 0;
 }
